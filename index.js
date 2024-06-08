20f6818a520971d2ec0c53d6141b31665036f8e5
sachinkumar2222 <sachin2322006@gmail.com>
2024-06-08 13:25:34 -0700

    day 1 js

---
+++ index.js
@@ -0,0 +1,122 @@
+//data type
+// //var
+// var num = 10;
+// {
+//   num=20;
+// }
+// console.log("var ",num);
+
+
+// //let
+// let num1=10;
+// {
+//   num1=20;
+// }
+// console.log("let ",num1);
+
+// //const 
+// const num2=10;
+// {
+//     num2=20;
+// }
+// console.log("const ",num2);
+// //var
+// var num = 10;
+// {
+//     var num = 20;
+//     num = 30;
+//     console.log(num);
+// }
+// console.log(num)
+// //let
+// let num2 = 10;
+// {
+//     let num2 = 20;
+//     num2 = 30;
+//     console.log(num2);
+// }
+// console.log(num2)
+// //const
+// const num3 = 10;
+// {
+//     const num3 = 20;
+//     num3 = 30;
+//     console.log(num3);
+// }
+// console.log(num3)
+
+
+//operator's
+// let num1 =10;
+// let num2 = 5;
+
+// let sum = num1+num2;
+// console.log("sum ",sum)
+
+// let sub = num1-num2;
+// console.log("sub ",sub)
+
+// let mul = num1*num2;
+// console.log("mul ",mul)
+
+// let div = num1/num2;
+// console.log("div ",div)
+
+// let mod = num1%num2;
+// console.log("mod ",mod)
+
+// num2 += num1
+// console.log(num2)
+
+// let increament = num2++ + num2 + num2++;
+// console.log(num2);
+// console.log(increament)
+
+// let i = 5;
+// let j = 10;
+// let incre = --i + ++j + i-- + j++;
+// console.log(i);
+// console.log(j);
+// console.log(incre);
+
+//logic operator
+
+// let checkIn = 10;
+// let checkOut = "10";
+// console.log(checkIn == checkOut);
+// console.log(checkIn === checkOut);
+// console.log(typeof checkIn);
+// console.log(typeof checkOut);
+
+// let chIn = true;
+// console.log(!chIn);
+
+// let checkIn = "";
+// let checkOut = 0;
+// console.log(!checkOut);
+
+// let checkOut = true;
+// // let checkIn = true;
+// let checkIn = false;
+// console.log(checkOut || checkIn);
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let greater = (a>b && a>c)?"a":(b>c)?"b":"c";
+// let less = (a<b && a<c)?"a":(b<c)?"b":"c";
+// console.log(greater)
+// console.log(less)
+
+
+// let age = prompt("enter your age1? ");
+// let vote = (age>=18)?"yes,u can vote":"no, u not eligible";
+// console.log(vote);
+
+// let num1 = {"key":"20"};
+// let arr = [1,2,3];
+// let result = (num1 && num2)?"yes":"no";
+// console.log(result);
+
+let result = 10 + 5 * 2;
+console.log(result);
